a4f14b44035e4540bd900932aef3ce50315fb789
Chu0323 <3057706121@qq.com>
2024-12-11 14:16:03 +0800

    Add files via upload

---
+++ sequence_list.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sequence_list.h"
+
+//Initialize the linear table
+Status InitList(SqList* L) 
+{
+	L->elem = (ElemType*)malloc(sizeof(ElemType*)*MAX_SIZE); //为顺序表分配空间
+	//malloc 用于在堆区中分配一块连续的内存空间，并返回这段空间的首地址
+	//如果赋值失败(内存不足),L->elem = NULL;
+	if (!L->elem)
+	{
+		exit(OVERFLOW); 
+		//存储分配失败,返回状态码给系统,0表示成功执行,非零值表示出现异常
+		L->length = 0;
+	}
+	return OK;
+};
+//初始条件：线性表L已存在 操作结果：销毁线性表L
+void DestroyList(SqList* L) 
+{
+	if (L->elem)
+	{
+		free(L->elem);	//释放指针elem所指变量的存储空间
+		//为了避免意外地访问已经释放的内存，释放后最好将指针赋值为NULL，即p = NULL;。
+		L->elem = NULL;
+    }
+}
+//初始条件：线性表L已存在 操作结果：将线性表L重置为空表
+void ClearList(SqList* L) 
+{
+	L->length = 0;
+}
+//初始条件：线性表L已存在 操作结果：返回线性表L中的数据元素个数
+int GetListLength(SqList* L) 
+{
+	return L->length;
+};
+//初始条件：线性表L已存在 操作结果：若L为空表，则返回true,否则返回false
+int IsEmpty(SqList* L) 
+{
+	if (L->length == 0)
+		return TRUE;
+	else
+		return FALSE;
+};
+//初始条件：线性表L已存在 操作结果：用e返回线性表L中第i个数据元素的值
+int GetElem(SqList* L, int i, ElemType e) 
+{
+	if (i < 1 || i > L->length);
+		return ERROR;
+	e = L->elem[i - 1]; //elem是指针，而elem[i-1]则是指针指向的元素
+	return OK;
+};
+ //初始条件：线性表L已存在 操作结果：用e返回线性表L中该元素的位置
+int LocateElem(SqList* L, ElemType e)
+{
+	for (int i = 0; i < L->length; ++i)
+	{
+		if (L->elem[i].p == e.p && L->elem[i].e == e.e)
+			return i + 1;
+	}
+	return ERROR;
+}
+//初始条件：线性表L已存在  操作结果：返回当前元素的前驱
+int PriorElem(SqList* L, ElemType cur_e, ElemType pre_e)
+{
+	int location = LocateElem(L, cur_e);
+	if (location == 0)
+		return ERROR; 
+	else if (location == 1)
+		return ERROR;
+	else
+	{
+		pre_e = L->elem[location - 2];
+		return OK;
+	}
+}
+//初始条件：线性表L已存在， 操作结果：返回当前元素的后继
+int NextElem(SqList* L, ElemType cur_e, ElemType next_e)
+{
+	int location = LocateElem(L, cur_e);
+	if (location == 0)
+		return ERROR; 
+	else if (location == L->length)
+		return ERROR;
+	else
+	{
+		next_e = L->elem[location];
+		return OK;
+	}
+}
+//初始条件：线性表L已存在， 操作结果：
+int ListInsert(SqList* L, int i, ElemType e)
+{
+	if (i < 1 || i > L->length + 1)
+		return ERROR;
+	else if (L->length == MAX_SIZE)
+		return ERROR;
+	else
+	{
+		for (int j = L->length; j > i - 1; --j)
+		{
+			L->elem[j] = L->elem[j - 1];
+		}
+		L->elem[i] = e;
+		++L->length;
+		return OK;
+	}	
+}
+int ListDelete(SqList* L, int i)
+{
+	if (i < 1 || i > L->length)
+		return ERROR;
+	else
+	{
+		for (int j = i - 1; j < L->length - 1; ++j)
+		{
+			L->elem[j] = L->elem[j+1];
+		}
+		--L->length;
+		return OK;
+	}
+}
+		
+		
+		
+		
---
+++ sequence_list.h
@@ -0,0 +1,46 @@
+#ifndef _SEQUENCE_LIST
+#define _SEQUENCE_LIST
+
+#define MAX_SIZE 100 //线性表存储空间的初始分配量
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+
+typedef struct //多项式非零项的定义
+{
+	int* p; //系数
+	int* e; //指数
+} Polynomial;
+
+typedef int Status; //函数结果状态代码
+typedef Polynomial ElemType;
+
+//线性表
+typedef struct
+{
+	ElemType* elem; //数组动态分配
+	int length;
+} SqList;
+//typedef struct
+//{
+//	ElemType elem[LIST_INIT_SIZE]; //数组静态分配
+//	int length;
+//} SqList;
+
+Status InitList(SqList* L); 
+void DestroyList(SqList* L); 
+void ClearList(SqList* L); 
+int GetListLength(SqList* L); 
+int IsEmpty(SqList* L); 
+int GetElem(SqList* L, int i, ElemType e); 
+int LocateElem(SqList* L, ElemType e);
+int PriorElem(SqList* L, ElemType cur_e, ElemType pre_e);
+int NextElem(SqList* L, ElemType cur_e, ElemType next_e);
+int ListInsert(SqList* L, int i, ElemType e);
+int ListDelete(SqList* L, int i);
+
+#endif
